78107f1811cc894b5372737771d3671ee817f036
Milan Hauth <milahu@gmail.com>
2022-10-31 14:31:47 +0100

    up

--- test/src/rules/react-types-to-solid-types.ts
+++ test/src/rules/react-types-to-solid-types.ts
@@ -1,3 +1,13 @@
 import React from "react"
 
 let onDragStart: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+
+let onDragEnd: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+
+function Button(props): React.ReactElement | null {
+  return (
+    <div>
+      <input type="button"/>
+    </div>
+  )
+}
