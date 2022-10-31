c651aacb343a4afdbe36e608e9a830d75f76c6e8
Milan Hauth <milahu@gmail.com>
2022-10-31 10:44:57 +0100

    up

--- test/import.sh
+++ test/import.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-find src/component-party/content/ -name react |
+find src/src/component-party/content/ -name react |
 while read reactDir
 do
   dir=$(dirname $reactDir)
-  path=${dir#*/} # remove src/ prefix
+  path=${dir#*/} # remove the first src/ prefix
   mkdir -p $path
   cp -r $dir/react $dir/solid $path
   find $path -type f | xargs sed -i 's|\t|  |g' # indent with spaces
