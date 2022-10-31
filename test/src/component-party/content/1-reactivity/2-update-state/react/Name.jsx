1ad1101ac26e1e48eedb65eb6009ff72e363bc01
Milan Hauth <milahu@gmail.com>
2022-10-31 11:30:38 +0100

    import tests from component-party

---
+++ test/src/component-party/content/1-reactivity/2-update-state/react/Name.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+
+export default function Name() {
+  const [name, setName] = useState('John');
+  setName('Jane');
+
+  return <h1>Hello {name}</h1>;
+}
---
+++ test/src/component-party/content/2-templating/6-conditional/solid/TrafficLight.jsx
@@ -0,0 +1,39 @@
+import { Switch, Match } from 'solid-js/web';
+import { createSignal } from 'solid-js';
+
+const TRAFFIC_LIGHTS = ['red', 'orange', 'green'];
+
+export default function TrafficLight() {
+  const [lightIndex, setLightIndex] = createSignal(0);
+
+  const light = () => TRAFFIC_LIGHTS[lightIndex()];
+
+  function nextLight() {
+    if (lightIndex() + 1 > TRAFFIC_LIGHTS.length - 1) {
+      setLightIndex(0);
+    } else {
+      setLightIndex(lightIndex() + 1);
+    }
+  }
+
+  return (
+    <>
+      <button onClick={nextLight}>Next light</button>
+      <p>Light is: {light()}</p>
+      <p>
+        You must
+        <Switch>
+          <Match when={light() === 'red'}>
+            <span>STOP</span>
+          </Match>
+          <Match when={light() === 'orange'}>
+            <span>SLOW DOWN</span>
+          </Match>
+          <Match when={light() === 'green'}>
+            <span>GO</span>
+          </Match>
+        </Switch>
+      </p>
+    </>
+  );
+}
