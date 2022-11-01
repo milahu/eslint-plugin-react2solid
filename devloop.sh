85b7956ad27c3926880501a7d589a10be7451ee1
Milan Hauth <milahu@gmail.com>
2022-11-01 19:44:25 +0100

    add

---
+++ devloop.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+src=test/src/react-query-devtools/src/devtools.tsx
+
+out=test/out/${src#test/src/}
+
+while true
+do
+
+echo
+date
+echo src $src
+echo out $out
+echo
+
+node test/index.js $src >/dev/null &&
+cat $out | grep 'React\.'
+
+echo
+echo done. to continue, hit enter
+read
+
+done
