ca63a705d3a4433b157de62b18668f4440dab918
Milan Hauth <milahu@gmail.com>
2022-11-01 22:07:05 +0100

    up

--- devloop.sh
+++ devloop.sh
@@ -2,6 +2,8 @@
 
 src=test/src/react-query-devtools/src/devtools.tsx
 
+src=test/src/react-forward-ref/react-forward-ref.tsx
+
 out=test/out/${src#test/src/}
 
 while true
@@ -13,8 +15,8 @@ echo src $src
 echo out $out
 echo
 
-node test/index.js $src >/dev/null &&
-cat $out | grep 'React\.'
+node test/index.js $src
+# >/dev/null && cat $out | grep 'React\.'
 
 echo
 echo done. to continue, hit enter
